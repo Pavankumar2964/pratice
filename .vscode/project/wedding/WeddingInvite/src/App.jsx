4c343280c1fa559b5c03ed3436c8fbfc7a1efa9a
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-12-08 22:03:29 +0530

    Project

---
+++ .vscode/project/wedding/WeddingInvite/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import WeddingInvite from './weddingInvite'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   <WeddingInvite/>
+    </>
+  )
+}
+
+export default App
