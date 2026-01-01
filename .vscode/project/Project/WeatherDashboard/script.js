78bdb35450208d4ee1c04ad1d82f63e46e99bee6
pavankumar2964 <pavankumar29ctr@gmail.com>
2026-01-01 21:49:53 +0530

    WeatherDashboard

---
+++ .vscode/project/Project/WeatherDashboard/script.js
@@ -0,0 +1,29 @@
+const apiKey = "YOUR_API_KEY_HERE";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    
+    if(response.status == 404) {
+        alert("City not found!");
+    } else {
+        var data = await response.json();
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+        // Update icon based on weather condition
+        const condition = data.weather[0].main.toLowerCase();
+        weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    }
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+});
