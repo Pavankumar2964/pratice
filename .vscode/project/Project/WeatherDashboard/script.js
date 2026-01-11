2d04aa3ca5f28d44d72750d733a6211e86206748
pavankumar2964 <pavankumar29ctr@gmail.com>
2026-01-11 21:43:26 +0530

    WeatherDashboard

--- .vscode/project/Project/WeatherDashboard/script.js
+++ .vscode/project/Project/WeatherDashboard/script.js
@@ -1,30 +1,36 @@
-const apiKey = "d61e48a4e33146ac99f124359260801";
-const apiUrl =" https:api.weatherapi.com/v1/forecast.json?key=YOUR_API_KEY&q=Hyderabad&days=3"
-;
+const apiKey = "629618e0b1769fd0b38e2f79303af93d"; // <-- put your key here
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
+    if (city === "") return;
+
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
-    if (response.status == 404) {
-        alert("City not found!");
-    } else {
-        var data = await response.json();
-
-        document.querySelector(".city").innerHTML = data.name;
-        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
-
-        // Update icon based on weather condition
-        const condition = data.weather[0].main.toLowerCase();
-        weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    if (response.status === 404) {
+        alert("City not found");
+        return;
     }
+
+    const data = await response.json();
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
 }
 
 searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value);
+});
+
+searchBox.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        checkWeather(searchBox.value);
+    }
 });
